4cca4fbac51c96c574b6d1097e94a43f98f80c1e
MD MOSTAFIZUR RAHMAN <fahim.dcc2017@gmail.com>
2025-03-10 10:41:36 +0000

    READY FOR PUBLISH

--- Live Clock/Program.cs
+++ Live Clock/Program.cs
@@ -1,29 +1,19 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    DefaultFileNames = new List<string> { "Index.html" } // Set your default file here
+});
 app.UseStaticFiles();
-
 
 app.UseAuthorization();
 
